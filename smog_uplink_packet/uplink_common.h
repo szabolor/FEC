00049619d4d8590f829ea21957c8a041b28afb5d
Lorant Szabo <szabolor@linux>
2016-08-19 13:54:51 +0200

    Cleanup uplink codec

---
+++ smog_uplink_packet/uplink_common.h
@@ -0,0 +1,20 @@
+#ifndef __UPLINK_CODEC_COMMON_H__
+#define __UPLINK_CODEC_COMMON_H__
+
+#define GEN_POLY   (0xAE3U)
+
+// Data size to be encoded (interpreted as payload length) 
+// [MUST BE DIVISIBLE BY 3]
+#define MSG_LEN    (31)
+
+// Encoder output size in bytes
+#define ENC_LEN    (63)
+
+// The amount of Golay code-words: ceil(MSG_LEN/1.5)
+#define WORD_COUNT (21)
+
+// Interleaver settings: 24x21 matrix
+#define INTERLEAVER_STEP_SIZE (21)
+#define INTERLEAVER_SIZE     (504)
+
+#endif
